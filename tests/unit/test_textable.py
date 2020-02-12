3bfd0cf3f69c1ba873e7a085457f442049ae513f
karlch <karlch@protonmail.com>
2020-02-12 18:00:05 +0100

    Add basic unit tests using pytest

---
+++ tests/unit/test_textable.py
@@ -0,0 +1,60 @@
+# vim: ft=python fileencoding=utf-8 sw=4 et sts=4
+
+"""Tests for textable._textable."""
+
+import pytest
+
+from textable import _textable
+
+
+def test_wrap_tex_environment():
+    text = "My custom text"
+    expected = f"\\begin{{center}}\n    {text}\n\\end{{center}}\n"
+    assert _textable._wrap_tex_environment("center", text) == expected
+
+
+def test_wrap_tex_environment_cmd():
+    text = "My custom text"
+    cmd = "lll"
+    expected = f"\\begin{{tabular}}{{{cmd}}}\n    {text}\n\\end{{tabular}}\n"
+    assert _textable._wrap_tex_environment("tabular", text, cmd=cmd) == expected
+
+
+def test_wrap_tex_environment_options():
+    text = "My custom text"
+    options = "lll"
+    expected = f"\\begin{{tabular}}[{options}]\n    {text}\n\\end{{tabular}}\n"
+    assert _textable._wrap_tex_environment("tabular", text, options=options) == expected
+
+
+@pytest.mark.parametrize(
+    "alignment, n_columns, expected",
+    (
+        ("l", 3, "lll"),
+        ("r", 2, "rr"),
+        ("c", 4, "cccc"),
+        ("l|", 3, "l|l|l"),
+        ("|l|", 3, "|l|l|l|"),
+        ("", 3, "ccc"),
+        ("llc", 3, "llc"),
+        ("|ll|l|", 3, "|ll|l|"),
+    )
+)
+def test_table_alignment(alignment, n_columns, expected):
+    assert _textable._table_alignment(alignment, n_columns) == expected
+
+
+def test_fail_table_alignment_chars():
+    with pytest.raises(ValueError, match="Invalid alignment"):
+        _textable._table_alignment("llb", 3)
+
+
+def test_fail_table_alignment_n_separators():
+    with pytest.raises(ValueError, match="Too many |"):
+        _textable._table_alignment("|l|||", 3)
+
+
+@pytest.mark.parametrize("alignment", ("ll", "llll"))
+def test_fail_table_alignment_n_chars(alignment):
+    with pytest.raises(ValueError, match="Number of alignment"):
+        _textable._table_alignment(alignment, 3)
