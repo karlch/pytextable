99b8a6e9055746f20b85fbf528eeddc271f608d1
karlch <karlch@protonmail.com>
2020-02-12 18:55:23 +0100

    Add integration tests
    The integration tests run the full write of textable and compare the
    results to existing files.

---
+++ tests/integration/test_textable.py
@@ -0,0 +1,43 @@
+# vim: ft=python fileencoding=utf-8 sw=4 et sts=4
+
+"""Integration tests for textable."""
+
+import os
+
+import pytest
+
+import textable
+
+
+DATA = [[1.2346, 1, 1.2346], [1.2346, 1.2346, 1.2346], [1.2346, 1.2346, 1.2346]]
+
+
+FILE_TO_KWARGS = {
+    "default.tex": {},
+    "threedigit.tex": {"fmt": ".3f"},
+    "threedigit_notrailing.tex": {"fmt": ".4g"},
+    "header.tex": {"header": ("first", "second", "third")},
+    "notable.tex": {"table": False},
+    "label.tex": {"label": "mylabel"},
+    "label_caption.tex": {"label": "mylabel", "caption": "My Caption"},
+    "nobooktabs.tex": {"booktabs": False},
+    "nobooktabs_header.tex": {
+        "booktabs": False,
+        "header": ("first", "second", "third"),
+    },
+}
+
+
+@pytest.mark.parametrize("infile, kwargs", FILE_TO_KWARGS.items())
+def test_textable(tmpdir, infile, kwargs):
+
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    infile = os.path.join(dirname, "data", infile)
+
+    def read_bytes(fname):
+        with open(fname, "rb") as f:
+            return f.read()
+
+    outfile = tmpdir.join("output.tex")
+    textable.write(DATA, outfile, **kwargs)
+    assert read_bytes(infile) == read_bytes(outfile)
